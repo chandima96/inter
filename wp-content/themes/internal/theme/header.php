f93ad1c7f481555541e11d57092934baf5dd5cd5
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-08-21 17:04:30 +0530

    Title

--- wp-content/themes/internal/theme/header.php
+++ wp-content/themes/internal/theme/header.php
@@ -17,15 +17,7 @@
 <head>
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="/dist/output.css" rel="stylesheet">
-
-
-
-
-
-
-
+    <title><?php wp_title() ?></title>
 	<?php wp_head(); ?>
 </head>
 
