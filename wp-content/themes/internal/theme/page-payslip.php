df21920c07499adb07f54c88bb240e6c5a4be03b
Kavindu <kavindu.seu.ft@gmail.com>
2023-08-21 11:22:45 +0530

    test

--- wp-content/themes/internal/theme/page-payslip.php
+++ wp-content/themes/internal/theme/page-payslip.php
@@ -12,7 +12,7 @@ get_header(); ?>
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/80x80-black1.png" alt="">
     </div>
     <div>
-        <h1 class="text-5xl font-chivo">Payslip Form</h1>
+        <h1 class="text-5xl font-chivo-h1">Payslip Form</h1>
     </div>
 </div>
 <!-- Header Section End -->
