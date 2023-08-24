6f98742d14f95e8954ad659199a53221594ef056
Kavindu <kavindu.seu.ft@gmail.com>
2023-08-24 18:21:24 +0530

    test

--- wp-content/themes/internal/theme/page-webdev.php
+++ wp-content/themes/internal/theme/page-webdev.php
@@ -98,15 +98,15 @@ get_header(); ?>
                                 <label class="text-xl font-normal font-poppins mb-1">Description 1</label>
                                 <input class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="description1" type="text" placeholder="Description">
                             </div>
-                            <div class="flex-auto w-10 text-center">
+                            <div class="flex-auto w-15 text-center">
                                 <label class="text-xl font-normal font-poppins mb-1">Quantity</label>
                                 <input class="border rounded w-full text-center font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="qty1" type="text">
                             </div>
-                            <div class="flex-auto w-20 text-center">
+                            <div class="flex-auto w-15 text-center">
                                 <label class="text-xl font-normal font-poppins mb-1">Unit Price</label>
                                 <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="unitP1" type="text">
                             </div>
-                            <div class="flex-auto w-20 text-end">
+                            <div class="flex-auto w-15 text-end">
                                 <label class="text-xl font-normal font-poppins mb-1">Total</label>
                                 <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total1" type="text">
                             </div>
