8e5b7e20cd3d7979b177254e9940675c2408ef6f
Kavindu <kavindu.seu.ft@gmail.com>
2023-08-24 18:19:25 +0530

    test

--- wp-content/themes/internal/theme/page-webdev.php
+++ wp-content/themes/internal/theme/page-webdev.php
@@ -94,21 +94,21 @@ get_header(); ?>
 
                 <div class="mb-11">
                         <div class="flex gap-10 mb-5">
-                            <div  class="flex-auto">
+                            <div  class="flex-none">
                                 <label class="text-xl font-normal font-poppins mb-1">Description 1</label>
                                 <input class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="description1" type="text" placeholder="Description">
                             </div>
-                            <div class="flex-auto w-5 text-center">
+                            <div class="flex-auto w-10 text-center">
                                 <label class="text-xl font-normal font-poppins mb-1">Quantity</label>
                                 <input class="border rounded w-full text-center font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="qty1" type="text">
                             </div>
-                            <div class="flex-auto w-10 text-center">
+                            <div class="flex-auto w-20 text-center">
                                 <label class="text-xl font-normal font-poppins mb-1">Unit Price</label>
-                                <input class="border rounded w-full text-center font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="unitP1" type="text">
+                                <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="unitP1" type="text">
                             </div>
-                            <div class="flex-auto w-10 text-end">
+                            <div class="flex-auto w-20 text-end">
                                 <label class="text-xl font-normal font-poppins mb-1">Total</label>
-                                    <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total1" type="text">
+                                <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total1" type="text">
                             </div>
                         </div>
     
@@ -127,7 +127,7 @@ get_header(); ?>
                             </div>
                             <div class="flex-auto w-10 text-end">
                                 <label class="text-xl font-normal font-poppins mb-1">Total</label>
-                                    <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total2" type="text">
+                                <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total2" type="text">
                             </div>
                         </div>
 
@@ -146,7 +146,7 @@ get_header(); ?>
                             </div>
                             <div class="flex-auto w-10 text-end">
                                 <label class="text-xl font-normal font-poppins mb-1">Total</label>
-                                    <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total3" type="text">
+                                <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total3" type="text">
                             </div>
                         </div>
 
@@ -165,7 +165,7 @@ get_header(); ?>
                             </div>
                             <div class="flex-auto w-10 text-end">
                                 <label class="text-xl font-normal font-poppins mb-1">Total</label>
-                                    <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total4" type="text">
+                                <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total4" type="text">
                             </div>
                         </div>
 
@@ -184,7 +184,7 @@ get_header(); ?>
                             </div>
                             <div class="flex-auto w-10 text-end">
                                 <label class="text-xl font-normal font-poppins mb-1">Total</label>
-                                    <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total5" type="text">
+                                <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total5" type="text">
                             </div>
                         </div>
 
@@ -203,7 +203,7 @@ get_header(); ?>
                             </div>
                             <div class="flex-auto w-10 text-end">
                                 <label class="text-xl font-normal font-poppins mb-1">Total</label>
-                                    <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total6" type="text">
+                                <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total6" type="text">
                             </div>
                         </div>
 
@@ -222,7 +222,7 @@ get_header(); ?>
                             </div>
                             <div class="flex-auto w-10 text-end">
                                 <label class="text-xl font-normal font-poppins mb-1">Total</label>
-                                    <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total7" type="text">
+                                <input class="border rounded w-full text-end font-semibold tabular-nums py-2 px-3 text-gray-700 leading-tight focus:border-sky-500 focus:outline-none focus:shadow-outline" id="total7" type="text">
                             </div>
                         </div>
                     </div>
