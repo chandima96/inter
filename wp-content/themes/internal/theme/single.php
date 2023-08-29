c91ade7050d8428dc6f0302fd5e5219b62286400
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-08-29 12:47:20 +0530

    Test

--- wp-content/themes/internal/theme/single.php
+++ wp-content/themes/internal/theme/single.php
@@ -10,12 +10,20 @@
 get_header();
 ?>
 
+<?php
+
+$blog_single_page_paragraph_one = get_field('blog_single_page_paragraph_one');
+$blog_single_page_paragrah_two = get_field('blog_single_page_paragrah_two');
+$blog_single_page_image = get_field('blog_single_page_image');
+
+?>
+
 <div class="bg-white py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
       <div class="lg:pr-4">
         <div class="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-64 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
-          <img class="absolute inset-0 h-full w-full object-cover brightness-125 saturate-0" src="https://images.unsplash.com/photo-1630569267625-157f8f9d1a7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80" alt="">
+          <img class="absolute inset-0 h-full w-full object-cover brightness-125 saturate-0" src="<?php echo $blog_single_page_image; ?>" alt="">
           <div class="absolute inset-0 bg-gray-900 mix-blend-multiply"></div>
           <div class="absolute left-1/2 top-1/2 -ml-16 -translate-x-1/2 -translate-y-1/2 transform-gpu blur-3xl" aria-hidden="true">
             <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-40" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
@@ -38,7 +46,8 @@ get_header();
           <p class="text-base font-semibold leading-7 text-indigo-600">Company values</p>
           <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"><?php echo get_the_title(); ?></h1>
           <div class="max-w-xl">
-            <p class="mt-6"><?php the_content(); ?></p>
+            <p class="mt-6"><?php echo $blog_single_page_paragraph_one; ?></p>
+            <p class="mt-6"><?php echo $blog_single_page_paragrah_two; ?></p>
           </div>
         </div>
         <dl class="mt-10 grid grid-cols-2 gap-8 border-t border-gray-900/10 pt-10 sm:grid-cols-4">
