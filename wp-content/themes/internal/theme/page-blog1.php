cfb8a12b43c2238354ccb67b7209ed4ff62eb546
Kavindu <kavindu.seu.ft@gmail.com>
2023-08-29 09:36:30 +0530

    2 blog pages added

---
+++ wp-content/themes/internal/theme/page-blog1.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	Template Name: Blog Page 1
+*/
+get_header(); ?>
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/internal/theme/page-blog2.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	Template Name: Blog Page 2
+*/
+get_header(); ?>
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
