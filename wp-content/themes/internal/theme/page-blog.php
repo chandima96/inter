645763a24b7d5bc53d5b20a76576960de7db1ed8
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-08-29 10:35:35 +0530

    Single

--- wp-content/themes/internal/theme/page-blog.php
+++ wp-content/themes/internal/theme/page-blog.php
@@ -25,13 +25,16 @@ get_header(); ?>
 										
 										?>
       <article class="flex flex-col items-start justify-between">
-        <div class="relative w-full"><?php 
+        <div class="relative w-full">
+			
+			<?php 
 if ( has_post_thumbnail() ) {
     $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
     //echo '<link rel="preload" as="image" href="' . esc_attr( $attachment_image ) . '">';  
 ?>
           <img src="<?php echo $attachment_image; ?>" alt="" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
 		  <?php } ?> 
+
 		  <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
         </div>
         <div class="max-w-xl">
@@ -41,7 +44,7 @@ if ( has_post_thumbnail() ) {
           </div>
           <div class="group relative">
             <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-              <a href="#">
+              <a href="<?php the_permalink(); ?>">
                 <span class="absolute inset-0"></span>
                 <?php the_title(); ?>
               </a>
