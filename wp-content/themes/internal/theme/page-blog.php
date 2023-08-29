22682be1b22d246efc84290ab3911d9682ef1700
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-08-29 10:16:37 +0530

    Test

--- wp-content/themes/internal/theme/page-blog.php
+++ wp-content/themes/internal/theme/page-blog.php
@@ -38,13 +38,21 @@ get_header(); ?>
             <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
               <a href="#">
                 <span class="absolute inset-0"></span>
-                Boost your conversion rate
+                <?php the_title(); ?>
               </a>
             </h3>
-            <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+            <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?php the_excerpt(); ?></p>
           </div>
           <div class="relative mt-8 flex items-center gap-x-4">
-            <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-100">
+
+		  <?php 
+			if ( has_post_thumbnail() ) {
+				$attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
+				//echo '<link rel="preload" as="image" href="' . esc_attr( $attachment_image ) . '">';  
+			?>
+            <img src="<?php echo $attachment_image; ?>" alt="" class="h-10 w-10 rounded-full bg-gray-100">
+			<?php } ?>
+
             <div class="text-sm leading-6">
               <p class="font-semibold text-gray-900">
                 <a href="#">
