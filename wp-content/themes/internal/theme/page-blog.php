050864bc288fb6463e95527bea07351baa43f2eb
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-08-29 10:18:40 +0530

    Test

--- wp-content/themes/internal/theme/page-blog.php
+++ wp-content/themes/internal/theme/page-blog.php
@@ -25,9 +25,14 @@ get_header(); ?>
 										
 										?>
       <article class="flex flex-col items-start justify-between">
-        <div class="relative w-full">
-          <img src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80" alt="" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
-          <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+        <div class="relative w-full"><?php 
+if ( has_post_thumbnail() ) {
+    $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
+    //echo '<link rel="preload" as="image" href="' . esc_attr( $attachment_image ) . '">';  
+?>
+          <img src="<?php echo $attachment_image; ?>" alt="" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
+		  <?php } ?> 
+		  <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
         </div>
         <div class="max-w-xl">
           <div class="mt-8 flex items-center gap-x-4 text-xs">
@@ -44,15 +49,8 @@ get_header(); ?>
             <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"><?php the_excerpt(); ?></p>
           </div>
           <div class="relative mt-8 flex items-center gap-x-4">
-
-		  <?php 
-			if ( has_post_thumbnail() ) {
-				$attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
-				//echo '<link rel="preload" as="image" href="' . esc_attr( $attachment_image ) . '">';  
-			?>
-            <img src="<?php echo $attachment_image; ?>" alt="" class="h-10 w-10 rounded-full bg-gray-100">
-			<?php } ?>
-
+			
+            <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="h-10 w-10 rounded-full bg-gray-100">
             <div class="text-sm leading-6">
               <p class="font-semibold text-gray-900">
                 <a href="#">
