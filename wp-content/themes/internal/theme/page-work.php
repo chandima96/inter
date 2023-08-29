174799b2562f7c322261c97402308c4c10e05dcd
Kavindu <kavindu.seu.ft@gmail.com>
2023-08-29 12:08:59 +0530

    work page added

--- wp-content/themes/internal/theme/page-work.php
+++ wp-content/themes/internal/theme/page-work.php
@@ -11,8 +11,27 @@ get_header(); ?>
       <p class="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert advice.</p>
     </div>
     <div class="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+    <?php $the_query = new WP_Query( array('post_type' =>'post','posts_per_page' => '6',  'post__not_in'   => array( $id),) );?>
+							     <?php if ( have_posts())   : while ( $the_query->have_posts() ) : $the_query->the_post();?>
+							  
+									   <?php
+										$thumbnail_id = get_post_thumbnail_id(); 
+										$thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
+										$thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true); 
+
+                    $categories = get_the_category();
+										
+										?>
       <article class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-        <img src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+
+      <?php 
+if ( has_post_thumbnail() ) {
+    $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
+    //echo '<link rel="preload" as="image" href="' . esc_attr( $attachment_image ) . '">';  
+?>
+        <img src="<?php echo $attachment_image; ?>" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+        <?php } ?> 
+
         <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
         <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
 
@@ -29,12 +48,13 @@ get_header(); ?>
           </div>
         </div>
         <h3 class="mt-3 text-lg font-semibold leading-6 text-white">
-          <a href="#">
+          <a href="<?php the_permalink(); ?>">
             <span class="absolute inset-0"></span>
-            Boost your conversion rate
+            <?php the_title(); ?>
           </a>
         </h3>
       </article>
+      <?php endwhile; endif; ?>
 
       <!-- More posts... -->
     </div>
